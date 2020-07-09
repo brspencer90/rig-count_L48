23ed5ee7e781ca1dc9007940f0fad177d1a5f087
Bryan S <bryan.r.spencer@gmail.com>
2020-07-09 13:51:24 -0500

    create cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Investigating the relationship between operating rig count in L48 and worldwide oil price, supply, and demand.',
+    author='Bryan S',
+    license='',
+)
